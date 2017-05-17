03f402c911b57ae779a7a762b630431f38f1f059
makevoid <makevoid@gmail.com>
2017-05-18 00:04:59 +0100

    refactor mixin

--- lib/bapp_models/eth_models/mixin.rb
+++ lib/bapp_models/eth_models/mixin.rb
@@ -1,6 +1,7 @@
 
 module BAppModels
   module EthModelMixin
+
     def update(attrs_new)
       klass = self.class
       model = klass.get id
@@ -8,15 +9,9 @@ module BAppModels
       attrs.merge! attrs_new
       obj   = klass.new attrs
       data  = Oj.dump obj.attributes
-      ETH["#{resource}:#{id}"] = data
+      ETH["#{self.class.resource}:#{id}"] = data
       obj
     end
 
-    private
-
-    def resource
-      model_name = self.class.name.downcase
-      Inflecto.pluralize model_name
-    end
   end
 end
