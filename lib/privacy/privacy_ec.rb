e68677d99ca03672c40d420764181d98663d9c30
makevoid <makevoid@gmail.com>
2017-06-07 20:25:13 +0100

    TODO

---
+++ lib/privacy/privacy_ec.rb
@@ -0,0 +1,12 @@
+# privacy via asymmetric encryption (public key cryptography) - Elliptic Curves - bitcoin secp-k1 (secp-256-k1) via c lib/bindings - TODO secp-zk-snarks
+
+
+# in your Gemfile
+#
+# gem "bitcoin-secp256k1", require: 'secp256k1' # lib-secp-k1 bindings (lib-secp-bitcoin - lib-secp)
+
+class PrivacyEC
+
+
+
+end
