4415d94e02cd1c64eb72d79c8f32d90ef6894b17
makevoid <makevoid@gmail.com>
2017-07-11 15:31:43 +0100

    add secp to travis ci

---
+++ spec/ci/install_secp.sh
@@ -0,0 +1,23 @@
+set -x
+set -e
+
+# sudo apt-get install -y libtool # already present on circle
+
+mkdir ~/tmp -p
+cd ~/tmp
+
+# skipping build and make for CI speed up
+if [ ! -d secp256k1 ]; then
+  git clone https://github.com/bitcoin-core/secp256k1
+fi
+
+cd secp256k1
+
+if [ ! -d .libs ]; then
+  ./autogen.sh
+  ./configure  --prefix=$HOME --enable-module-recovery
+
+  make
+fi
+
+make install
