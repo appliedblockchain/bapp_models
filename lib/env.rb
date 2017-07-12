3531cb9d881b392a1eb1ea91c9bf8a351bfa4a4d
Andrew Campbell <andycampbell92@gmail.com>
2017-07-12 11:08:20 +0100

    removing KV_MULTI by default on DEV

--- lib/env.rb
+++ lib/env.rb
@@ -4,7 +4,7 @@ end
 
 KV_MULTI = ENV["KV_MULTI"] == "1"
 
-if BAppModels::RACK_ENV == "development"
-  Object.send *%i(remove_const KV_MULTI)
-  KV_MULTI = true
-end
+# if BAppModels::RACK_ENV == "development"
+#   Object.send *%i(remove_const KV_MULTI)
+#   KV_MULTI = true
+# end
